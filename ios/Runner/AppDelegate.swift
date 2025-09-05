17ff1beccb7887144520e0ab485238da34cf5502
saleh <sabwlwy99991@gmail.com>
2025-09-06 00:17:34 +0200

    Add after udate test2

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,26 @@
-import Flutter
 import UIKit
+import Flutter
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    
+    // تأخير تسجيل Plugins حتى يكتمل تهيئة FlutterEngine
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
+    
+    // تهيئة المشتقات الأخرى أولاً
+    self.window.makeKeyAndVisible()
+    
+    // تسجيل الـ plugins بشكل آمن
+    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+      GeneratedPluginRegistrant.register(with: self)
+    }
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
